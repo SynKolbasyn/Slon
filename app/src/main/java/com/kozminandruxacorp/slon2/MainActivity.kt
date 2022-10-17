040e1468b4149d6e0ce0492c21c13005946cd18d
Andrew <syn.kolbasyn.06@gmail.com>
2022-10-17 20:03:46 +0300

    v0.1.0 Добавлен интерфейс

--- app/src/main/java/com/kozminandruxacorp/slon2/MainActivity.kt
+++ app/src/main/java/com/kozminandruxacorp/slon2/MainActivity.kt
@@ -2,10 +2,19 @@ package com.kozminandruxacorp.slon2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initViews()
+    }
+
+    private fun initViews(){
+        main_latitude_tv.text = "Latitude"
+        main_longitude_tv.text = "Longitude"
+        main_latitude_val.text = "0"
+        main_longitude_val.text = "0"
     }
 }
